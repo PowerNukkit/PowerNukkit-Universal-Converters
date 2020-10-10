3fc4abb95228e19da8b13475a5237f2612175949
joserobjr <joserobjr@powernukkit.org>
2020-10-10 00:07:22 -0300

    Initialize the project and creates a partial mapping

---
+++ Module.md
@@ -0,0 +1,7 @@
+# Module universal-world-converter
+
+This is an example of how you can write module documentation with Dokka.
+
+# Package org.powernukkit.universal.block
+
+This package contains a few examples of Dokka usage.
---
+++ src/main/kotlin/org/powernukkit/universal/WorldConverterCLI.kt
@@ -0,0 +1,36 @@
+/*
+ *     PowerNukkit Universal Worlds & Converters for Minecraft
+ *     Copyright (C) 2020  José Roberto de Araújo Júnior
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.powernukkit.universal
+
+/**
+ * Executes the world conversion from the system's command line.
+ * 
+ * @author joserobjr
+ * @since 2020-10-09
+ */
+object WorldConverterCLI {
+    /**
+     * The entry point of the command line interface.
+     * @param args The arguments that was given in the command line.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        TODO("Implement")
+    }
+}
