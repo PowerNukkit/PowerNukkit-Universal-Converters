2ebddd74483637cc15f42ee2d5e8fd943b2a0f31
joserobjr <joserobjr@powernukkit.org>
2020-10-15 20:04:51 -0300

    Optimize JavaBlock and fixes a bug with BedrockBlock

--- src/main/kotlin/org/powernukkit/converters/platform/bedrock/block/BedrockBlock.kt
+++ src/main/kotlin/org/powernukkit/converters/platform/bedrock/block/BedrockBlock.kt
@@ -1,19 +1,19 @@
 /*
  * PowerNukkit Universal Worlds & Converters for Minecraft
- *  Copyright (C) 2020  José Roberto de Araújo Júnior
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *  
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (C) 2020  José Roberto de Araújo Júnior
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.powernukkit.converters.platform.bedrock.block
@@ -35,7 +35,7 @@ class BedrockBlock(
     blockEntity: BedrockBlockEntity? = null,
     entities: MutableList<BedrockEntity> = mutableListOf(),
 ) : BaseBlock<BedrockPlatform, BedrockBlockState, BedrockBlockEntity, BedrockEntity>(
-    platform, pos
+    platform, pos, blockEntity, entities
 ) {
     override val blockLayers = listOf(mainState, secondaryState)
 }
