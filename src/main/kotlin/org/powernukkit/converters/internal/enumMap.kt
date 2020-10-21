31891acaaa993be5d9aad877474bfb1f8b24f904
joserobjr <joserobjr@powernukkit.org>
2020-10-21 19:46:15 -0300

    Setups github workflows

--- src/main/kotlin/org/powernukkit/converters/internal/enumMap.kt
+++ src/main/kotlin/org/powernukkit/converters/internal/enumMap.kt
@@ -1,17 +1,17 @@
 /*
  * PowerNukkit Universal Worlds & Converters for Minecraft
  * Copyright (C) 2020  José Roberto de Araújo Júnior
- *   
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *   
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *   
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -67,9 +67,5 @@ inline fun <reified K: Enum<K>, V> enumMapOfNonNullsOrEmpty(vararg pairs: Pair<K
         return emptyMap()
     }
 
-    val map = EnumMap<K,V>(K::class.java)
-    for (pair in pairs) {
-        pair?.let(map::plusAssign)
-    }
-    return map
+    return enumMapOfNonNulls(*pairs)
 }
--- src/main/kotlin/org/powernukkit/converters/internal/enumSet.kt
+++ src/main/kotlin/org/powernukkit/converters/internal/enumSet.kt
@@ -1,17 +1,17 @@
 /*
  * PowerNukkit Universal Worlds & Converters for Minecraft
  * Copyright (C) 2020  José Roberto de Araújo Júnior
- *   
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *   
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *   
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -81,9 +81,5 @@ inline fun <reified E: Enum<E>> enumSetOfNonNullsOrEmpty(vararg values: E?): Set
         return emptySet()
     }
     
-    val set = enumSetOf<E>()
-    for (value in values) {
-        value?.let(set::plusAssign)
-    }
-    return set
+    return enumSetOfNonNulls(*values)
 }
